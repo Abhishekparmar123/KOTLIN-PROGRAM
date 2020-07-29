5764d5493ccd4d231d0952a60089c717a6780d63
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2020-07-29 10:32:19 +0530

    Array in kotlin

---
+++ array.kt
@@ -0,0 +1,13 @@
+fun main()
+{
+    var characterArray = arrayOf('a', 'b', 'c', 'd')
+    val intArray = arrayOf(1, 2, 3, 4, 5)
+    characterArray[3]= 'z'
+
+    println(characterArray[1])
+    println(intArray[2])
+
+    val stringArray = arrayOf("hello ", "kotlin ", "programmer ")
+    print(stringArray[1])
+
+}

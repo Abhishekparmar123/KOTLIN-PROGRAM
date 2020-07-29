5099b78978962c920a28996ae9cbceabd6d18aa8
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2020-07-29 10:33:06 +0530

    if else in kotlin

---
+++ if_else.kt
@@ -0,0 +1,19 @@
+fun main()
+{
+    val a = 10
+    val b = 10
+
+
+    if (a>b)
+    {
+        print("$a is greater than $b")
+    }
+    else if(a==b)
+    {
+        print("Both are equal .. ")
+    }
+    else
+    {
+        print("$b is greater than $a")
+    }
+}

4058c49a9d3f266dbbfdc1d26c6f49fde7d4c354
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2020-08-14 11:49:35 +0530

    Add files via upload

---
+++ loopForStep.kt
@@ -0,0 +1,9 @@
+//increment in loops
+
+fun main()
+{
+    for (i in 1..10 step 2)
+    {
+        println(i)
+    }
+}
---
+++ loopForTable.kt
@@ -0,0 +1,11 @@
+// gives table of two
+
+fun main()
+{
+    val a = 2
+
+    for(i in 1..10)
+    {
+        println(i*a)
+    }
+}

bdbc3ee121bb06efe6b3ef77ba2f1eaea481b84d
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2020-08-21 12:08:04 +0530

    String template in kotlin

--- string_template.kt
+++ string_template.kt
@@ -2,6 +2,6 @@ fun main()
 {
     val s = "Welcome to intershala "
 
-    // we use dollar sign as tenplate
+    // we use dollar sign as template
     print("The 9th no character is ${s.get(9)}")
 }
